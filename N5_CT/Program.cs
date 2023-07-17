be9d2551464a3943ea89b68457aeac1017243ec8
Firdavs Asadov <toshmurodovj13@gmail.com>
2023-07-17 04:45:26 -0700

    1- vazifa buldi

--- N5_CT/Program.cs
+++ N5_CT/Program.cs
@@ -1,2 +1,83 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//string[] Names = new string[]
+//    {
+//        "Abdulxayev Jasurbek Anvar O'g'li",
+//        "Abdumannonov Botirjon Davlat O'g'li",
+//        "Abdurahmonov Azizbek Alisher O'g'li",
+//        "Anvarjonov Ozodbek Avazxon O'g'li",
+//        "Asadov Firdavs Asqarbek O'g'li",
+//        "Eshmurodov Umarali Abduhalim O'g'li",
+//        "Ibrohimjonov Shodiyor Ziyodali O'g'li",
+//        "Jo'raboyev Boburjon Murodjon O'g'li",
+//        "Kamolov Bunyod Maruf O'g'li",
+//        "Karimjonov Ilhom Ramzjon O'g'li",
+//        "Komiljonov Muhammadaziz Abduraximovich",
+//        "Rashidov Asadbek",
+//        "Sadriddinov Abdurahmon Jurabek O'g'li",
+//        "Sattorova Habiba Shuxrat Qizi",
+//        "Tolibov Firdavs Odil O'g'li",
+//        "To'rayev Dinur Alisher O'g'li",
+//        "Vositov Muhammadrizo Muhammadtolib O'g'li",
+//        "Xaybullayev Mexroj Mansurjonovich",
+//        "Xolmuratov Qurbonali Suxrob O'g'li",
+//    };
+//Console.WriteLine("Search: ");
+//string search = Console.ReadLine().ToLower();
+//foreach (string name in Names)
+//{
+//    if (name.ToLower().Contains(search)) 
+//    {
+//        Console.WriteLine(name);
+//    }
+//}
+var changedwords = "{ApplicationDate} {ApplicationNumber} { OrganizationName}rektori " +
+    "{ RektorgaAbituriyent ${StudentName} danTel.: ${ StudentNumber}ARIZAMen joriy " +
+    "yilda ${OrganizationName}ning ${ Code}- ${ EduSpeciality}ta 'lim yo'nalishini " +
+    "${ EduForm}ta 'lim shakli, ${EduLanguage} ta'lim tili " +
+    "bo'yicha ${OrderId} - OTM sifatida tanlab, " +
+    "davlat test sinovida ishtirok etdim.Davlat test markazi tomonidan " +
+    "e'lon qilingan test natijasiga ko'ra ${Ball} " +
+    "ball to'pladim va to'lov-kontrakt asosidagi qabul " +
+    "chegarasiga ${DifferenceBall} ball yetmay qoldi .Shu munosabat bilan, " +
+    "meni qo'shimcha tarzda ${EduContractSumType} tabaqalashtirilgan to'lov-kontrakt " +
+    "asosida talabalikka qabul qilishingizni " +
+    "hamda menga to`lov shartnomasini taqdim etishingizni so'rayman.Men Oliy ta'lim muassasasining " +
+    "ichki tartib qoidalari va " +
+    "kontraktni barcha shartlari bilan tanishib chiqdim hamda tasdiqlangan o'quv rejasi bo'yicha " +
+    "belgilangan vaqtlarda " +
+    "darslarga qatnashishga, belgilangan muddatlarda to'lov-kontrakt pulini to'lashga va to'lov hujjatlarini topshirishga " +
+    "shaxsan o`zim mas'ulligimni tasdiqlayman.Abituriyent: ${ Abiturient}ID raqami: ${ AbiturientId}";
+var ApplicationDate = "{ApplicationDate}";
+var ApplicationNumber = "{ApplicationNumber}";
+var OrganizationName = "{OrganizationName}";
+var Rektor = "{Rektor}";
+var StudentName = "{StudentName}";
+var Studentnumber = "{StudentNumber}";
+var Code = "{Code}";
+var EduSpeciality = "{EduSpeciality}";
+var EduForm = "{EduForm}";
+var EduLanguage = "{EduLanguage}";
+var OrderId = "{OrderId}";
+var Ball = "{Ball}";
+var DifferenceBall = "{DifferenceBall}";
+var EduContractSumType = "{EduContractSumType}";
+var Abituriyent = "{Abituriyent}";
+var AbituriyentId = "{AbituriyentId}";
+var formattedNotifications = changedwords
+    .Replace(ApplicationDate, DateTime.Now.ToString())
+    .Replace(ApplicationNumber, Guid.NewGuid().ToString())
+    .Replace(OrganizationName, "Najot Ta'lim")
+    .Replace(Rektor, "Temurbek Adhamov")
+    .Replace(StudentName, "Firdavs Asadov")
+    .Replace(Studentnumber, "+998888055831")
+    .Replace(Code, "Backend N10")
+    .Replace(EduSpeciality, "Dasturlash")
+    .Replace(EduForm, "bootcamp-result")
+    .Replace(EduLanguage, "Uzbek")
+    .Replace(OrderId, "8540")
+    .Replace(Ball, "100")
+    .Replace(DifferenceBall, "-1")
+    .Replace(EduContractSumType, "1000000")
+    .Replace(Abituriyent, "Firdavs Asadov")
+    .Replace(AbituriyentId, "1111");
+Console.WriteLine(formattedNotifications);
+       
--- N5_HT/Program.cs
+++ N5_HT/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("ismingizni kiriting: ");
+//var ism = Console.ReadLine();
+//Console.WriteLine("sharifingizni kiriting: ");
+//var sharif = Console.ReadLine();
+//var formmattingname = string.Format(ism + sharif).ToLower();
+//Console.WriteLine(formmattingname);
+//var names = "qurbonaliholmuratov firdavsasadov abduraahmonsadriddinov firdavstolibov fazliddinomonov".Split();
+//var simbols = "& ! @ # $ % ^ & * () _ - + = ".Split();
+//Random random = new Random();
+//var randomsymbol = random.Next(simbols.Length);
+//if (names.Contains(formmattingname))
+//{
+//    formmattingname += simbols[randomsymbol];
+//    Console.WriteLine(formmattingname);
+//}
+//else
+//{
+//    Console.WriteLine("siz umumman hato username kiritdingiz");
+//}
+
+var essaybals = 500;
+var essay = "Lorem ipsum dolor sit amet consectetur adipisicing elit. quaerat est " +
+    "quas commodi quibusdam labore, nihil doloribus quam temporibus " +
+    "inventore optio expedita consectetur voluptatem QUIDEM nulla soluta earum." +
+    " Numquam rem alias minima culpa iste distinctio. Eum similique est consequuntur minus, odio " +
+    "nisi recusandae iure asperiores facere, reiciendis voluptate maiores! Repellat, dolorum!".Split();
+int sozlarSoni = SozlarSoni(essay);
+if (sozlarSoni < 500)
+{
+    essaybals -= 5;
+}
+else
+{
+    essaybals += 5;
+}
+static int SozlarSoni(string essay)
+{
+    string[] sozlar = essay.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+}
